4a22ec8794dedf0ff6f60b8f1382efcddbdb1901
Matte <matteo24giu@gmail.com>
2025-08-02 01:02:04 +0200

    edited

--- Monopoli_2/main.py
+++ Monopoli_2/main.py
@@ -5,21 +5,28 @@ import random
 import json
 
 patrimonio=200
-imprevisti_path = r"C:\Users\alessandrini\Documents\coding\data python\monopoli\imprevisti.json"
-board_path =r"C:\Users\alessandrini\Documents\coding\data python\monopoli\monopoli_board.json"
+imprevisti_path = r"C:\Users\alessandrini\Documents\coding\games\Monopoli_2\imprevisti.json"
+board_path =r"C:\Users\alessandrini\Documents\coding\games\Monopoli_2\monopoli_board.json"
 
-folder = "monopoli"
+folder = "Monopoli_2"
 os.makedirs(folder, exist_ok=True)
 nome_file = 'monopoli_spese.xlsx'
 exel_path = os.path.join(folder, nome_file)
 
 def verifica_file():
     try:
-        if os.path.exists(imprevisti_path) and os.path.exists(board_path):
+        if os.path.exists(imprevisti_path) and os.path.exists(board_path) and os.path.exists(exel_path):
             print("📁 i file esistonno !")
             return True
         else:
             print("❌ File inesistenti.")
+
+            wb = Workbook()
+            ws=wb.sheetnames["giocatori"]
+            ws = wb.active
+            ws.title = "Giocatori"
+            ws.append(["Giocatori", "importo"])
+
             return False
     except Exception as e:
         print(f"❌ Errore nel controllo dei file: {e}")
@@ -39,14 +46,14 @@ def carica_board():
     except json.JSONDecodeError:
         return []
     
-lista_giocatori = []
+
 def aggiungi_giocatori():
+    lista_giocatori = []
     try:
         num_players = int(input("quanti giocatori ? "))
     except ValueError:
         print("❌ inserisci numero valido")
         return aggiungi_giocatori()
-    
     for i in range(num_players):
         while True:
             giocatore = input(f"inserisci numero giocatore {i+1}: ").lower().strip()
@@ -59,22 +66,39 @@ def aggiungi_giocatori():
                 break
             else:
                 print("❌ Invalid name. Try again.")
-
     print(f"Registered players: {lista_giocatori}")
+    return lista_giocatori
+
+def apri_json():
+    with open(board_path, 'r' , "utf-8") as file:
+        return json.load(file)
     
+
+def lancia_dadi(lista_giocatori,board):
+    posizione_iniziale=0
+    numero_dati = int(input("inserisci numero dadi :"))
+    if numero_dati >12:
+        print("❌ Non puoi fare piu di 12 con 2 dadi ")
+        return lancia_dadi(lista_giocatori,board)
+    
+    for posto in board:
+        if posto["posizione"] == numero_dati:
+            print(f"giocatore, atterrato nella casella {posto['nome']} , descrizione : {posto['tipo']} , colore {posto['colore']}")
+
+
+
 def main():
-    esiste = verifica_file()
-    aggiungi_giocatori()
-    if esiste:
-        imprevisti = carica_imprevisti()
-        board = carica_board()
-        wb=Workbook()
-        ws=wb.active
-        ws.title= "Giocatori"
-        ws.append(["Giocatori", "importo"])
-        for giocatore in lista_giocatori:
-            ws.append([giocatore,patrimonio])
-        wb.save(exel_path)
+    esiste= verifica_file()
+    if not esiste:
+        lista_giocatori = aggiungi_giocatori()
+    imprevisti = carica_imprevisti()
+    board = carica_board()
+
+    wb = load_workbook(exel_path)
+    ws = wb["Giocatori"]
+    lista_giocatori = [row[0] for row in ws.iter_rows(min_row=2, max_col=1, values_only=True) if row[0]]
+
+    lancia_dadi(lista_giocatori,board)
 
 
 
